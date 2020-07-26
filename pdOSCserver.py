af7d0fa454f83749785214f016375bd1d3ed54bf
Styles <68823538+stylesmunson@users.noreply.github.com>
2020-07-26 15:36:51 -0700

    Modified @tupperward's modified version of my code
    it works now!

--- pdOSCserver.py
+++ pdOSCserver.py
@@ -1,6 +1,3 @@
-# import argparse
-# import math
-
 from pythonosc.osc_server import BlockingOSCUDPServer
 from pythonosc.dispatcher import Dispatcher
 from pythonosc.udp_client import SimpleUDPClient
@@ -12,17 +9,7 @@ red = LED(17)
 green = LED(18)
 switchButton = Button(26, hold_time = 3)
 red.on()
-currentLEDstatus = "red on"
 
-def redOn():
-    if not red.is_lit():
-        red.on()
-        green.off()
-
-def greenOn():
-    if not green.is_lit():
-        green.on()
-        red.off()
 
 # OSC CLIENT
 
@@ -42,15 +29,19 @@ print ("*** starting OSC server ***")
 
 def record_mode(address, *args):
     print(f"{address}: {args}")
-    redOn
+    red.on()
+    green.off()
 
 def play_mode(address, *args):
     print(f"{address}: {args}")
-    greenOn
+    red.off()
+    green.on()
 
 def save_lights_start(address, *args):
-    currentLEDred = red.is_lit()
+    currentLEDstatus = red.is_lit
     print(f"{address}: {args}")
+    red.off()
+    green.off()
     red.blink(on_time = 0.1, off_time = 0.2, n = 5, background = False)
     green.blink(on_time = 0.1, off_time = 0.2, n = 5, background = False)
     if currentLEDstatus:
@@ -69,7 +60,6 @@ dispatcher = Dispatcher()
 dispatcher.map("/record", record_mode)
 dispatcher.map("/play", play_mode)
 dispatcher.map("/saveStart", save_lights_start)
-#dispatcher.map("/saveEnd", save_lights_stop)
 dispatcher.set_default_handler(default_handler)
 
 server = BlockingOSCUDPServer((ip, serverPort), dispatcher)
